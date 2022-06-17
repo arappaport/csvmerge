6a934125eca80b25b333a0928ad81877a8aac561
arappaport <>
2022-06-17 13:33:11 -0400

    Updated usage notes

--- README.md
+++ README.md
@@ -1,13 +1,46 @@
 # csvmerge
 
-Join two csv files on a specific column.  Command line wrapper around Pandas merge
-defaults to outer join
+Join two csv files on a specific column.  Command line wrapper around Pandas merge. 
+  Defaults to outer join
     
-Usage:
+##Usage
 
-   Outer Join two files on a column named 'id'
-   ```poetry run python ./src/csvmerge.py ./data/left.csv ./data/right.csv  id --join outer --output output_output.csv --verbose```
+Usage: csvmerge.py [OPTIONS] LEFT RIGHT
+
+   Join 2 csv files on a specific column.
+
+   defaults to outer join
+
+   Usage:
+
+  Left Join two files on a column named 'id' 
+  
+```
+poetry run python ./src/csvmerge.py ./data/left.csv ./data/right.csv  --join left --left_on id
+  --output left_output.csv --verbose
+```
+
+  Outer Join two files.   Left file join on column named 'id', right file on
+  'alt_id' 
+  
+```
+poetry run python ./src/csvmerge.py ./data/left.csv
+  ./data/right.csv  --join outer --left_on id  --right_on alt_id --output
+  outer_output.csv --verbose
+  ```
 
 
-   Left Join two files on a column named 'id'
-   ```poetry run python ./src/csvmerge.py ./data/left.csv ./data/right.csv  id --join left --output left_output.csv --verbose```
+
+Usage Options: <br>
+  --left_on TEXT                  Column to join on from left file <br>
+  --right_on TEXT                 Column to join on from right file.  If not,
+                                  present then the --left_on is used <br>
+  --join [outer|inner|left|right]
+                                  join type: inner, outer.   Defaults to outer <br>
+  -o, --output TEXT               (optional) name of file to write data into.<br>
+  -d, --dryrun                    If set, will only gather data and not write<br>
+  -v, --verbose                   Enables verbose logging mode<br>
+  -q, --quiet                     Limits logging output to warnings and
+                                  errors.<br>
+  --version                       Show the version and exit. <br>
+  --help                          Show this message and exit.<br>
--- src/csvmerge.py
+++ src/csvmerge.py
@@ -8,7 +8,8 @@ LOGGER = logging.getLogger(__name__)
 @click.command()
 @click.argument('left',type=click.Path(exists=True))
 @click.argument('right',type=click.Path(exists=True))
-@click.argument('merge_col')
+@click.option( '--left_on', help="Column to join on from left file")
+@click.option( '--right_on', help="Column to join on from right file.  If not, present then the --left_on is used")
 @click.option( '--join', default='outer', help='join type: inner, outer.   Defaults to outer',
               type=click.Choice(['outer', 'inner', 'left', 'right']))
 
@@ -28,7 +29,11 @@ def cli(ctx, **kwargs):
     Usage:
 
    Left Join two files on a column named 'id'
-   '''poetry run python ./src/csvmerge.py ./data/left.csv ./data/right.csv  id --join left --output left_output.csv --verbose'''
+   '''poetry run python ./src/csvmerge.py ./data/left.csv ./data/right.csv  --join left --left_on id  --output left_output.csv --verbose'''
+
+   Outer Join two files.   Left file join on column named 'id', right file on 'alt_id'
+   '''poetry run python ./src/csvmerge.py ./data/left.csv ./data/right.csv  --join outer --left_on id  --right_on alt_id --output outer_output.csv --verbose'''
+
 
     """
     ctx.ensure_object(dict)
@@ -49,8 +54,12 @@ def cli(ctx, **kwargs):
     LOGGER.debug("File[%s] has %d rows excluding header", kwargs.get('left'), len(df_left))
     LOGGER.debug("File[%s] has %d rows excluding header", kwargs.get('right'), len(df_right))
 
+    left_on = kwargs.get('left_on')
+    right_on = kwargs.get('right_on') or left_on
+
     #defaults to inner
-    df = pd.merge(df_left, df_right, on=merge_col, how=how, suffixes=('_left', '_right'))
+    df = pd.merge(df_left, df_right, left_on=left_on, right_on=right_on,
+                  on=merge_col, how=how, suffixes=('_left', '_right'))
     df.fillna('',inplace=True)
     LOGGER.debug("After merge. df has %d rows excluding header", len(df))
 
@@ -61,7 +70,7 @@ def cli(ctx, **kwargs):
         df.to_csv(output,  encoding='utf-8', index=False)
         LOGGER.info("Output to file[%s]", output)
 
-    #TODO merge col doesn;t exist in both csvs
+    #TODO Test merge col doesn;t exist in both csvs
 
 if __name__ == '__main__':
     cli(obj={})
